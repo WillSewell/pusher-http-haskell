fe269e30318f5a67ebabed26c1f20748b9d70508
Samuel Yallop <samuel.yallop@pusher.com>
2017-08-01 16:26:13 +0100

    Tests encoding/decoding of randomised Notifications

--- test/Push.hs
+++ test/Push.hs
@@ -1,14 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Push where
 
 import qualified Data.Aeson as A
 import Data.Aeson ((.=))
+import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.HashMap.Strict as HM
 import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
 import qualified Data.Text as T
 
-import Test.Hspec (Spec, describe, it, shouldBe)
-import Test.QuickCheck (elements, property, Gen, vectorOf, oneof)
+import Test.Hspec (Spec, describe, it)
+import Test.QuickCheck
+       (elements, property, Gen, vectorOf, oneof, Arbitrary(..))
 
 import Network.Pusher
 
@@ -29,11 +34,28 @@ test = do
           case mkInterest txt of
             Nothing -> True
             Just _ -> False
-  describe "Notifications parse" $
-    it "Notification JSON parses correctly" $
-    property $
-    let (inputBS, expected) = validFCMDecoding
-    in A.decode inputBS == Just expected
+  describe "Notifications parse" $ do
+    it "A specific FCM Notification JSON parses correctly" $
+      property $
+      let (inputBS, expected) = validFCMDecoding
+      in A.decode inputBS == Just expected
+    it "Random Notifications JSON parses correctly" $
+      property $ \(NotificationDecoding (bs, notification)) ->
+        A.decode bs == Just notification
+  describe "Notifications encode" $ do
+    it "A specific FCM Notification encodes to JSON correctly" $
+      property $
+      let (bs, notification) = validFCMDecoding
+      in (A.encode notification) ==
+         (A.encode . fromJust . (A.decode :: ByteString -> Maybe Notification) $
+          bs)
+    it "Random Notifications encode to JSON correctly" $
+      -- NOTE: We put the expected string through a round trip to normalise trivial
+      -- differences such as ordering and spacing in the JSON Aeson generates
+      property $ \(NotificationDecoding (bs, notification)) ->
+        (A.encode notification) ==
+        (A.encode . fromJust . (A.decode :: ByteString -> Maybe Notification) $
+         bs)
 
 -- A single example of a FCM notification which we expect to aeson decode to
 -- the paired Notification value.
@@ -70,6 +92,80 @@ validFCMDecoding =
               ]
         }
   in (bs, notification)
+
+-- | A ByteString will decode to some value 'a'.
+type Decoding a = (B.ByteString, a)
+
+newtype NotificationDecoding =
+  NotificationDecoding (Decoding Notification)
+  deriving (Show)
+
+newtype APNSDecoding =
+  APNSDecoding (Decoding (Maybe APNSPayload))
+
+newtype FCMDecoding =
+  FCMDecoding (Decoding (Maybe FCMPayload))
+
+newtype GCMDecoding =
+  GCMDecoding (Decoding (Maybe GCMPayload))
+
+instance Arbitrary APNSDecoding where
+  arbitrary = return $ APNSDecoding ("", Nothing)
+
+instance Arbitrary GCMDecoding where
+  arbitrary = return $ GCMDecoding ("", Nothing)
+
+instance Arbitrary FCMDecoding where
+  arbitrary = return $ FCMDecoding ("", Nothing)
+
+instance Arbitrary NotificationDecoding where
+  arbitrary = do
+    interestName <- arbitraryInterestText
+    APNSDecoding (apnsBS, mAPNS) <- arbitrary
+    GCMDecoding (gcmBS, mGCM) <- arbitrary
+    FCMDecoding (fcmBS, mFCM) <- arbitrary
+    let notification =
+          Notification
+          { notificationInterest = fromJust . mkInterest $ interestName
+          , notificationWebhookURL = Nothing
+          , notificationWebhookLevel = Nothing
+          , notificationAPNSPayload = mAPNS
+          , notificationGCMPayload = mGCM
+          , notificationFCMPayload = mFCM
+          }
+        -- Key value pairs that are required
+        requiredFields :: [(ByteString, ByteString)]
+        requiredFields =
+          [("interests", "[\"" <> (B.pack . T.unpack $ interestName) <> "\"]")]
+        -- A function which takes an initial key value pair list and adds Just pairs and skips Nothing values.
+        -- Aeson would by default encode this as "value":null which we dont want.
+        consOptionals :: [(ByteString, ByteString)]
+                      -> [(ByteString, ByteString)]
+        consOptionals =
+          consJust "apns" (nullToMaybe apnsBS) .
+          consJust "fcm" (nullToMaybe fcmBS) .
+          consJust "gcm" (nullToMaybe gcmBS)
+        fields :: [(ByteString, ByteString)]
+        fields = consOptionals requiredFields
+        bs :: B.ByteString
+        bs =
+          (\acc -> "{" <> acc <> "}") .
+          B.intercalate "," . map (\(k, v) -> "\"" <> k <> "\": " <> v) $
+          fields
+    return $ NotificationDecoding (bs, notification)
+      -- Cons an attribute value pair if Just
+    where
+      consJust
+        :: ByteString
+        -> Maybe ByteString
+        -> [(ByteString, ByteString)]
+        -> [(ByteString, ByteString)]
+      consJust attr = maybe id (\val rest -> (attr, val) : rest)
+      -- Empty ByteStrings become null
+      nullToMaybe :: B.ByteString -> Maybe B.ByteString
+      nullToMaybe bs
+        | B.null bs = Nothing
+        | otherwise = Just bs
 
 -- Valid interest names
 arbitraryInterestText :: Gen T.Text
