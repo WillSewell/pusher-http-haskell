6588f7fbe468a3483b699bd92ee52f8abba8d3a2
Will Sewell <me@willsewell.com>
2020-05-29 23:01:02 +0100

    Drop library/ghc support to avoid use of cpp

--- example/api/Main.hs
+++ example/api/Main.hs
@@ -1,12 +1,8 @@
-{-# LANGUAGE CPP #-}
-
 module Main where
 
 import Control.Exception (displayException)
 import qualified Data.HashSet as HS
-#if !(MIN_VERSION_base(4,11,0))
 import Data.Monoid ((<>))
-#endif
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Yaml as Y
